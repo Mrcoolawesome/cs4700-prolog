a25f0644beb0fe8f4df2d6ddad69b2e9e19c9331
Devin Schutz <schutzdevin@gmail.com>
2026-03-06 14:29:36 -0700

    really done this time

--- castle.pl
+++ castle.pl
@@ -19,13 +19,13 @@ solveRooms(Castle, L) :-
 
 % helper to actually traverse the graph
 % this is a helper base case, where we're at the exit and the given room list is empty
-traverse(Castle, exit, [], [exit]) :-
+traverse(_Castle, exit, [], [exit]) :-
   true. % we did it if we made it here
 
 % recursive case 1 for helper: if the next room matches the head of our list
 traverse(Castle, CurrentRoom, GivenList, [CurrentRoom | PathTail]) :-
   % find a connected room that matches the head item of our list
-  room(Castle, CurrentRoom, NextRoom, Cost),
+  room(Castle, CurrentRoom, NextRoom, _Cost),
 
   % this will search the full given list for the CurrentRoom, and if it does find it it will give us the list without that element
   select(CurrentRoom, GivenList, NewGivenList),
@@ -36,7 +36,7 @@ traverse(Castle, CurrentRoom, GivenList, [CurrentRoom | PathTail]) :-
 % recursive case 2 for helper: if the next room doesn't match the first item in the list
 traverse(Castle, CurrentRoom, GivenList, [CurrentRoom | PathTail]) :-
   % find a connected room
-  room(Castle, CurrentRoom, NextRoom, Cost),
+  room(Castle, CurrentRoom, NextRoom, _Cost),
 
   % make sure that the next room doesn't equal any of the elements in the given list
   \+ memberOfX(CurrentRoom, GivenList),
@@ -46,7 +46,7 @@ traverse(Castle, CurrentRoom, GivenList, [CurrentRoom | PathTail]) :-
 
 % recursive case 3 for helper: keep going until we get to the exit even if the given list is empty
 traverse(Castle, CurrentRoom, [], [CurrentRoom | PathTail]) :-
-  room(Castle, CurrentRoom, NextRoom, Cost),
+  room(Castle, CurrentRoom, NextRoom, _Cost),
   traverse(Castle, NextRoom, [], PathTail).
 
 % helper to determine if an element is a member of a list
@@ -65,7 +65,7 @@ solveRoomsWithinCost(Castle, Limit) :-
 % helper for finding if a path exists that's under the limit
 % base case: if we made it to the end check if the path was under the limit 
 % prolog will just traverse every path on its own through the recursion in the recursive case so i just need to specify what paths i do want to return true for
-traverseWithCost(Castle, exit, Limit, OriginalLimit, [exit]) :- % also put the exit into the return list
+traverseWithCost(_Castle, exit, Limit, OriginalLimit, [exit]) :- % also put the exit into the return list
   Limit >= 0, % make sure its remaning limit is greater than 0
 
   % calculate what we spent
